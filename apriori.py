aea96f03dae6a257531ef25ce02c8fc5d8a2921b
armanexplorer <armanexplorer@gmail.com>
2021-05-10 20:10:58 +0430

    get frequent itemset completed

--- apriori.py
+++ apriori.py
@@ -1,27 +1,69 @@
-
-import pandas as pd
-import numpy as np
-import os
-
-
 class Arules:
     def __init__(self):
         pass
 
     def get_frequent_item_sets(self, transactions=None, min_support=None, min_confidence=None):
+        ans = []
         d = dict()
-        num = len(transactions)
-        for r in transactions:
-            for w in r:
-                if w == '':
+        min_num = len(transactions) * min_support
+        for trans_index, trans in enumerate(transactions):
+            for item in trans:
+                if item == '':
                     break
-                d[w] = d.get(w, 0) + 1
-        w = num * min_support
+                d[(item,)] = d.get((item,), set())
+                d[(item,)].add(trans_index)
+        
         for k, v in d.copy().items():
-            if(v<w):
+            if len(v) < min_num:
                 del d[k]
-        return d
+            else:
+                ans.append(k)
+        
+        list_of_last_dicts = [d]
+        num = len(d)
+        while(num > 0):
+            num = 0
+            list_of_cur_dicts = []
+            for dic in list_of_last_dicts:
+                for i1, k1 in enumerate(dic.keys()):
+                    dd = dict()
+                    for k2 in list(dic)[i1+1:]:
+                        new_key = k1+(k2[-1],)
+                        rep_trans = dic[k1].intersection(dic[k2])
+                        if len(rep_trans) > min_num:
+                            dd[new_key] = rep_trans
+                            ans.append(new_key)
+                    list_of_cur_dicts.append(dd)
+                    num+=len(dd)
+            list_of_last_dicts = list_of_cur_dicts
+        return ans
+            
 
     def get_arules(self, min_support=None, min_confidence=None, min_lift=None, sort_by='lift'):
         pass
     # sort_by: lift , confidence, support
+
+
+    # def get_details(self, transactions=None, min_support=None, min_confidence=None):
+    #     d = dict()
+    #     n = list()
+    #     num = len(transactions)
+    #     for i, r in enumerate(transactions):
+    #         s = 0
+    #         for w in r:
+    #             if w == '':
+    #                 break
+    #             d[w] = d.get(w, {"number": 0, "trans": []})
+    #             d[w]['trans'].append(i)
+    #             d[w]['number'] += 1
+    #             # print(d[w])
+
+    #             s+=1
+    #         if s>0:
+    #             n.append(s)
+    #     w = num * min_support
+    #     for k, v in d.copy().items():
+    #         if(v['number']<w):
+    #             del d[k]
+    #     # print(d)
+    #     return d,n
--- test.py
+++ test.py
@@ -2,12 +2,38 @@ from pandas.core.frame import DataFrame
 from apriori import Arules
 import pandas as pd
 
+MAIN_FILE_NAME = 'Project1 - groceries.csv'
+TEST_FILE_NAME = 'Project1 - groceries copy.csv'
+MIN_SUPPORT = 0.005
+MIN_CONFIDENCE = 0.2
 
-df:DataFrame = pd.read_csv('Project1 - groceries.csv', header = None, squeeze = True, keep_default_na=False)
+df:DataFrame = pd.read_csv(MAIN_FILE_NAME, header = None, squeeze = True, keep_default_na=False)
 a = Arules()
-d = a.get_frequent_item_sets(df.values, min_support=0.005, min_confidence=0.2)
-print(d)
-print(len(d))
+res=a.get_frequent_item_sets(transactions=df.values, min_support=MIN_SUPPORT, min_confidence=MIN_CONFIDENCE)
+print(res)
+
+
+
+
+# print(res[-1])
+# min_num = len(df.values) * MIN_SUPPORT
+# for r in res:
+#     num = 0
+#     for v in df.values:
+#         if len(set(v).intersection(set(r))) == len(r):
+#             num+=1
+#     print(num)
+
+
+# res = a.get_details(df.values, min_support=0.005, min_confidence=0.2)
+# d = res[0] # dict of objects and a object of number and trans id of occurunces
+# n = res[1] # list of number of elements for each transaction
+# # print(n)
+# print(f"number of transactions are: {len(df.values)}")
+# print(f"number of distinct objects are: {len(d)}")
+# print(f"number of all objects are: {sum(obj['number'] for obj in d.values())}")
+# print(f"max number of transactions that an object occurs: {max(len(obj['trans']) for obj in d.values())}")
+# print(f"sum of binary search cost for finding an object in transactions: {sum(np.log(n))}")
 
 
 # df:DataFrame = pd.read_csv('Project1 - groceries.csv', header=None, keep_default_na=False)
