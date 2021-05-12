1c14f72289c33426d2c77cb175ce0d00683bac01
armanexplorer <armanexplorer@gmail.com>
2021-05-12 21:40:08 +0430

    draw diagrams

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from pandas.core.frame import DataFrame
 from apriori import Arules
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+from PyARMViz.Rule import generate_rule_from_dict
+import PyARMViz
 
 MAIN_FILE_NAME = 'Project1 - groceries.csv'
 TEST_FILE_NAME = 'Project1 - groceries copy.csv'
@@ -14,8 +18,75 @@ a.get_frequent_item_sets(transactions=df.values, min_support=MIN_SUPPORT)
 print(f'number of frequent itemstes: {len(a.frequent_itemsets)}')
 arules = a.get_arules(min_confidence=MIN_CONFIDENCE)
 print(f'number of association rules: {len(arules)}\n')
-for rule in arules:
-    print('%s -> %s\t\t\t:::::::\t%s' % rule)
+# for rule in arules:
+#     print('{%s -> %s, %s}' % rule)
+# print(arules)
+# arules = [[1,2,3,4,5],(1,2,3,4,5),(1,2,3,4,5),(1,2,3,4,5),(1,2,3,4,5),(1,2,3,4,5)]
+# plt.scatter(['ali', 'arman','ghasemi','mohseni','reza','aghareza'],[a[2] for a in arules] ,alpha=0.5)
+
+# scatter
+plt.scatter([r['support'] for r in arules],[r['confidence'] for r in arules] ,alpha=0.5)
+plt.xlabel('support')
+plt.ylabel('confidence')
+plt.title('Support vs Confidence')
+plt.show()
+
+
+# parallel diagrams
+rules = []
+for rd in arules: 
+    rules.append(generate_rule_from_dict(rd))
+PyARMViz.generate_parallel_category_plot(rules)
+
+
+# fig, ax = plt.subplots()
+# cmap = matplotlib.cm.coolwarm
+# items = [{k: v} for k,v in a.num_of_occurrences.items() if len(k) == 1]
+# mini = min(items["Count"])
+# maxi = max(items["Count"])
+
+# norm = matplotlib.colors.Normalize(vmin=mini, vmax=maxi)
+# colors = [cmap(norm(value)) for value in item["Count"]]
+
+# squarify.plot(sizes=item["Count"], label=item["Item"], alpha=0.8, color=colors)
+# plt.axis('off')
+# plt.title("Top 50 Frequent Basket Items", fontsize=32)
+# ttl = ax.title
+# ttl.set_position([.5, 1.05])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ######################### Test that our code is true #########################
