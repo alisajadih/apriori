7604068b81652b390b265c699eeb657a88ed89b8
armanexplorer <armanexplorer@gmail.com>
2021-05-12 19:52:58 +0430

    last changes

--- apriori.py
+++ apriori.py
@@ -1,51 +1,77 @@
+from operator import itemgetter
+from itertools import combinations, chain
+
+
 class Arules:
     def __init__(self):
-        pass
+        self.frequent_itemsets = list()
+        self.num_of_occurrences = dict()
+        self.num_of_transactions = 0
 
-    def get_frequent_item_sets(self, transactions=None, min_support=None, min_confidence=None):
-        ans = []
-        d = dict()
-        min_num = len(transactions) * min_support
+    def get_frequent_item_sets(self, transactions=None, min_support=None):
+        self.num_of_transactions = len(transactions)
+        req_num_of_occurrences = self.num_of_transactions * min_support
+        one_itemsets_dict = dict()
         for trans_index, trans in enumerate(transactions):
             for item in trans:
                 if item == '':
                     break
-                d[(item,)] = d.get((item,), set())
-                d[(item,)].add(trans_index)
+                one_itemsets_dict[(item,)] = one_itemsets_dict.get((item,), set())
+                one_itemsets_dict[(item,)].add(trans_index)
         
-        for k, v in d.copy().items():
-            if len(v) < min_num:
-                del d[k]
+        for k, v in one_itemsets_dict.copy().items():
+            if len(v) < req_num_of_occurrences:
+                del one_itemsets_dict[k]
             else:
-                ans.append(k)
+                self.frequent_itemsets.append(k)
+                self.num_of_occurrences[k] = len(v)
         
-        list_of_last_dicts = [d]
-        num = len(d)
-        while(num > 0):
-            num = 0
+        list_of_last_dicts = [one_itemsets_dict]
+        frequent_k_itemsets_num = len(one_itemsets_dict)
+        while(frequent_k_itemsets_num > 0):
+            frequent_k_itemsets_num = 0
             list_of_cur_dicts = []
             for dic in list_of_last_dicts:
                 for i1, k1 in enumerate(dic.keys()):
-                    dd = dict()
+                    starts_with_dict = dict()
                     for k2 in list(dic)[i1+1:]:
                         new_key = k1+(k2[-1],)
-                        rep_trans = dic[k1].intersection(dic[k2])
-                        if len(rep_trans) > min_num:
-                            dd[new_key] = rep_trans
-                            ans.append(new_key)
-                    list_of_cur_dicts.append(dd)
-                    num+=len(dd)
+                        repeat_in_transactions = dic[k1].intersection(dic[k2])
+                        if len(repeat_in_transactions) > req_num_of_occurrences:
+                            starts_with_dict[new_key] = repeat_in_transactions
+                            self.frequent_itemsets.append(new_key)
+                            self.num_of_occurrences[new_key] = len(repeat_in_transactions)
+                    list_of_cur_dicts.append(starts_with_dict)
+                    frequent_k_itemsets_num+=len(starts_with_dict)
             list_of_last_dicts = list_of_cur_dicts
-        return ans
+
+        return self.frequent_itemsets
             
 
-    def get_arules(self, min_support=None, min_confidence=None, min_lift=None, sort_by='lift'):
-        pass
+    def get_arules(self, min_support=0, min_confidence=0, min_lift=0, sort_by='lift'):
+        arules = []
+        for itemset in self.frequent_itemsets:
+            subsets = list(chain.from_iterable(combinations(itemset, r) for r in range(1, len(itemset))))
+            num_of_itemset_repeat = self.num_of_occurrences[itemset]
+            support = num_of_itemset_repeat / self.num_of_transactions
+            if support < min_support:
+                continue
+            for index, left_side in enumerate(subsets, start=1):
+                right_side = subsets[len(subsets) - index]
+                confidence = num_of_itemset_repeat / self.num_of_occurrences[left_side]
+                if confidence < min_confidence:
+                    continue
+                lift = confidence * self.num_of_transactions / self.num_of_occurrences[right_side]
+                if lift < min_lift:
+                    continue
+                arules.append((left_side, right_side, eval(sort_by)))
+        arules = sorted(arules, key=itemgetter(2), reverse=True)
+        return arules
     # sort_by: lift , confidence, support
 
 
     # def get_details(self, transactions=None, min_support=None, min_confidence=None):
-    #     d = dict()
+    #     one_itemsets_dict = dict()
     #     n = list()
     #     num = len(transactions)
     #     for i, r in enumerate(transactions):
@@ -53,17 +79,17 @@ class Arules:
     #         for w in r:
     #             if w == '':
     #                 break
-    #             d[w] = d.get(w, {"number": 0, "trans": []})
-    #             d[w]['trans'].append(i)
-    #             d[w]['number'] += 1
-    #             # print(d[w])
+    #             one_itemsets_dict[w] = one_itemsets_dict.get(w, {"number": 0, "trans": []})
+    #             one_itemsets_dict[w]['trans'].append(i)
+    #             one_itemsets_dict[w]['number'] += 1
+    #             # print(one_itemsets_dict[w])
 
     #             s+=1
     #         if s>0:
     #             n.append(s)
     #     w = num * min_support
-    #     for k, v in d.copy().items():
+    #     for k, v in one_itemsets_dict.copy().items():
     #         if(v['number']<w):
-    #             del d[k]
-    #     # print(d)
-    #     return d,n
+    #             del one_itemsets_dict[k]
+    #     # print(one_itemsets_dict)
+    #     return one_itemsets_dict,n
--- test.py
+++ test.py
@@ -6,51 +6,47 @@ MAIN_FILE_NAME = 'Project1 - groceries.csv'
 TEST_FILE_NAME = 'Project1 - groceries copy.csv'
 MIN_SUPPORT = 0.005
 MIN_CONFIDENCE = 0.2
+# MIN_LIFT = 
 
 df:DataFrame = pd.read_csv(MAIN_FILE_NAME, header = None, squeeze = True, keep_default_na=False)
 a = Arules()
-res=a.get_frequent_item_sets(transactions=df.values, min_support=MIN_SUPPORT, min_confidence=MIN_CONFIDENCE)
-print(res)
+a.get_frequent_item_sets(transactions=df.values, min_support=MIN_SUPPORT)
+print(f'number of frequent itemstes: {len(a.frequent_itemsets)}')
+arules = a.get_arules(min_confidence=MIN_CONFIDENCE)
+print(f'number of association rules: {len(arules)}\n')
+for rule in arules:
+    print('%s -> %s\t\t\t:::::::\t%s' % rule)
 
 
+######################### Test that our code is true #########################
+# num = 0
+# t_num = len(df.values)
+# for rule in arules:
+#     num1=0
+#     num2=0
+#     num3=0
 
-
-# print(res[-1])
-# min_num = len(df.values) * MIN_SUPPORT
-# for r in res:
-#     num = 0
-#     for v in df.values:
-#         if len(set(v).intersection(set(r))) == len(r):
-#             num+=1
-#     print(num)
-
-
-# res = a.get_details(df.values, min_support=0.005, min_confidence=0.2)
-# d = res[0] # dict of objects and a object of number and trans id of occurunces
-# n = res[1] # list of number of elements for each transaction
-# # print(n)
-# print(f"number of transactions are: {len(df.values)}")
-# print(f"number of distinct objects are: {len(d)}")
-# print(f"number of all objects are: {sum(obj['number'] for obj in d.values())}")
-# print(f"max number of transactions that an object occurs: {max(len(obj['trans']) for obj in d.values())}")
-# print(f"sum of binary search cost for finding an object in transactions: {sum(np.log(n))}")
-
-
-# df:DataFrame = pd.read_csv('Project1 - groceries.csv', header=None, keep_default_na=False)
-# r = df.shape[0]
-# print(df.values)
-# all = df.to_records(col)
-# print(all[0])
-# print(df[:][:1])
-# print(df.unique())
-# k1 = []
-# for i in range(1,10):
-#     print(df[:][[i]])
-    # print(df[:][:1])
-    # k1 += list(df[col].unique())
-# print(len(k1))
-# print(df.count())
-# for a in df.head():
-#     print(a)
-# print(df.head())
-# print(df)
+#     for t in df.values:
+#         # print(set(rule[0]), set(rule[1]), set(t))
+#         t1=False
+#         t2=False
+#         if len(set(rule[0]).intersection(set(t))) == len(rule[0]):
+#             t1 = True
+#             num1+=1
+#         if len(set(rule[1]).intersection(set(t))) == len(rule[1]):
+#             num2+=1
+#             t2 = True
+#         if t1 and t2:
+#             num3+=1
+#     # print(num1, num2, num3)
+#     sup1 = num1/t_num
+#     sup2 = num2/t_num
+#     # print(num1,num2)
+#     sup = num3/t_num
+#     conf = num3/num1
+#     lift = conf*t_num/num2
+#     if sup >= MIN_SUPPORT and conf >= MIN_CONFIDENCE:
+#         num+=1
+#     print(lift, rule[2])
+    
+# print(num)
