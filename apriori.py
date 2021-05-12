8eda70e2bc1e7a8bff127a502efdeace5cd4aae4
alisajadih <44581771+alisajadih@users.noreply.github.com>
2021-05-13 00:03:02 +0430

    Algo (#1)
    * Create README.md
    
    * get frequent itemset completed
    
    * last changes
    
    * draw diagrams
    
    * arul.ipynb added
    
    Co-authored-by: Arman MZD <44166374+armanexplorer@users.noreply.github.com>
    Co-authored-by: armanexplorer <armanexplorer@gmail.com>

--- apriori.py
+++ apriori.py
@@ -1,27 +1,108 @@
-
-import pandas as pd
-import numpy as np
-import os
+from operator import itemgetter
+from itertools import combinations, chain
 
 
 class Arules:
     def __init__(self):
-        pass
+        self.frequent_itemsets = list()
+        self.num_of_occurrences = dict()
+        self.num_of_transactions = 0
 
-    def get_frequent_item_sets(self, transactions=None, min_support=None, min_confidence=None):
-        d = dict()
-        num = len(transactions)
-        for r in transactions:
-            for w in r:
-                if w == '':
+    def get_frequent_item_sets(self, transactions=None, min_support=None):
+        self.num_of_transactions = len(transactions)
+        req_num_of_occurrences = self.num_of_transactions * min_support
+        one_itemsets_dict = dict()
+        for trans_index, trans in enumerate(transactions):
+            for item in trans:
+                if item == '':
                     break
-                d[w] = d.get(w, 0) + 1
-        w = num * min_support
-        for k, v in d.copy().items():
-            if(v<w):
-                del d[k]
-        return d
+                one_itemsets_dict[(item,)] = one_itemsets_dict.get((item,), set())
+                one_itemsets_dict[(item,)].add(trans_index)
+        
+        for k, v in one_itemsets_dict.copy().items():
+            if len(v) < req_num_of_occurrences:
+                del one_itemsets_dict[k]
+            else:
+                self.frequent_itemsets.append(k)
+                self.num_of_occurrences[k] = len(v)
+        
+        list_of_last_dicts = [one_itemsets_dict]
+        frequent_k_itemsets_num = len(one_itemsets_dict)
+        while(frequent_k_itemsets_num > 0):
+            frequent_k_itemsets_num = 0
+            list_of_cur_dicts = []
+            for dic in list_of_last_dicts:
+                for i1, k1 in enumerate(dic.keys()):
+                    starts_with_dict = dict()
+                    for k2 in list(dic)[i1+1:]:
+                        new_key = k1+(k2[-1],)
+                        repeat_in_transactions = dic[k1].intersection(dic[k2])
+                        if len(repeat_in_transactions) > req_num_of_occurrences:
+                            starts_with_dict[new_key] = repeat_in_transactions
+                            self.frequent_itemsets.append(new_key)
+                            self.num_of_occurrences[new_key] = len(repeat_in_transactions)
+                    list_of_cur_dicts.append(starts_with_dict)
+                    frequent_k_itemsets_num+=len(starts_with_dict)
+            list_of_last_dicts = list_of_cur_dicts
 
-    def get_arules(self, min_support=None, min_confidence=None, min_lift=None, sort_by='lift'):
-        pass
+        return self.frequent_itemsets
+            
+
+    def get_arules(self, min_support=0, min_confidence=0, min_lift=0, sort_by='lift'):
+        arules = []
+        for itemset in self.frequent_itemsets:
+            subsets = list(chain.from_iterable(combinations(itemset, r) for r in range(1, len(itemset))))
+            num_of_itemset_repeat = self.num_of_occurrences[itemset]
+            support = num_of_itemset_repeat / self.num_of_transactions
+            if support < min_support:
+                continue
+            for index, left_side in enumerate(subsets, start=1):
+                right_side = subsets[len(subsets) - index]
+                confidence = num_of_itemset_repeat / self.num_of_occurrences[left_side]
+                if confidence < min_confidence:
+                    continue
+                lift = confidence * self.num_of_transactions / self.num_of_occurrences[right_side]
+                if lift < min_lift:
+                    continue
+                arules.append((left_side, right_side, eval(sort_by)))
+                # arules.append((left_side, right_side, support, confidence, lift))
+                # arules.append({
+                #     "lhs": left_side,
+                #     "rhs": right_side,
+                #     "count_full": num_of_itemset_repeat,
+                #     "count_lhs": self.num_of_occurrences[left_side],
+                #     "count_rhs": self.num_of_occurrences[right_side],
+                #     "num_transactions": self.num_of_transactions,
+                #     "support": support,
+                #     "confidence": confidence,
+                #     "lift": lift
+                # })
+        arules = sorted(arules, key=itemgetter(2), reverse=True)
+        # arules = sorted(arules, key=itemgetter(4), reverse=True)
+        return arules
     # sort_by: lift , confidence, support
+
+
+    # def get_details(self, transactions=None, min_support=None, min_confidence=None):
+    #     one_itemsets_dict = dict()
+    #     n = list()
+    #     num = len(transactions)
+    #     for i, r in enumerate(transactions):
+    #         s = 0
+    #         for w in r:
+    #             if w == '':
+    #                 break
+    #             one_itemsets_dict[w] = one_itemsets_dict.get(w, {"number": 0, "trans": []})
+    #             one_itemsets_dict[w]['trans'].append(i)
+    #             one_itemsets_dict[w]['number'] += 1
+    #             # print(one_itemsets_dict[w])
+
+    #             s+=1
+    #         if s>0:
+    #             n.append(s)
+    #     w = num * min_support
+    #     for k, v in one_itemsets_dict.copy().items():
+    #         if(v['number']<w):
+    #             del one_itemsets_dict[k]
+    #     # print(one_itemsets_dict)
+    #     return one_itemsets_dict,n
--- test.py
+++ test.py
@@ -1,30 +1,136 @@
 from pandas.core.frame import DataFrame
 from apriori import Arules
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+from PyARMViz.Rule import generate_rule_from_dict
+import PyARMViz
+import squarify
 
+MAIN_FILE_NAME = 'Project1 - groceries.csv'
+TEST_FILE_NAME = 'Project1 - groceries copy.csv'
+# MIN_SUPPORT = 0.055
+MIN_SUPPORT = 0.005
+MIN_CONFIDENCE = 0.2
+# MIN_CONFIDENCE = 0.4
+# MIN_LIFT = 
 
-df:DataFrame = pd.read_csv('Project1 - groceries.csv', header = None, squeeze = True, keep_default_na=False)
+df:DataFrame = pd.read_csv(MAIN_FILE_NAME, header = None, squeeze = True, keep_default_na=False)
 a = Arules()
-d = a.get_frequent_item_sets(df.values, min_support=0.005, min_confidence=0.2)
-print(d)
-print(len(d))
+a.get_frequent_item_sets(transactions=df.values, min_support=MIN_SUPPORT)
+print(f'number of frequent itemstes: {len(a.frequent_itemsets)}')
+arules = a.get_arules(min_confidence=MIN_CONFIDENCE)
+print(f'number of association rules: {len(arules)}\n')
+for rule in arules:
+    print('{%s -> %s, %s}' % rule)
+    # qq = (rule[0], rule[1], rule[4])
+# print(arules)
+# arules = [[1,2,3,4,5],(1,2,3,4,5),(1,2,3,4,5),(1,2,3,4,5),(1,2,3,4,5),(1,2,3,4,5)]
+# plt.scatter(['ali', 'arman','ghasemi','mohseni','reza','aghareza'],[a[2] for a in arules] ,alpha=0.5)
+
+# scatter
+# plt.scatter([r['support'] for r in arules],[r['confidence'] for r in arules] ,alpha=0.5)
+# plt.xlabel('support')
+# plt.ylabel('confidence')
+# plt.title('Support vs Confidence')
+# plt.show()
 
 
-# df:DataFrame = pd.read_csv('Project1 - groceries.csv', header=None, keep_default_na=False)
-# r = df.shape[0]
-# print(df.values)
-# all = df.to_records(col)
-# print(all[0])
-# print(df[:][:1])
-# print(df.unique())
-# k1 = []
-# for i in range(1,10):
-#     print(df[:][[i]])
-    # print(df[:][:1])
-    # k1 += list(df[col].unique())
-# print(len(k1))
-# print(df.count())
-# for a in df.head():
-#     print(a)
-# print(df.head())
-# print(df)
+# parallel diagrams
+# rules = []
+# for rd in arules: 
+#     rules.append(generate_rule_from_dict(rd))
+# print(len(rules))
+# PyARMViz.generate_parallel_category_plot(rules)
+# PyARMViz.generate_rule_graph_plotly(rules)
+# PyARMViz.generate_rule_strength_plot(rules)
+
+
+# item = {"Count": [], "Item": []}
+# # print(sorted(a.num_of_occurrences.items(), reverse=True, key=lambda item: item[1])[:50])
+# for el in sorted(a.num_of_occurrences.items(), reverse=True, key=lambda item: item[1])[:50]:
+#     if len(el[0]) ==1:
+#         item["Item"].append(el[0][0])
+#         item["Count"].append(el[1])
+# fig, ax = plt.subplots()
+# cmap = matplotlib.cm.coolwarm
+# mini = min(item["Count"])
+# maxi = max(item["Count"])
+
+# norm = matplotlib.colors.Normalize(vmin=mini, vmax=maxi)
+# colors = [cmap(norm(value)) for value in item["Count"]]
+# # print(colors)
+# squarify.plot(sizes=item["Count"], label=item["Item"], alpha=0.8, color=colors)
+# plt.axis('off')
+# plt.title("Top 50 Frequent Basket Items", fontsize=15)
+# plt.show()
+# ttl = ax.title
+# ttl.set_position([.5, 1.05])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################### Test that our code is true #########################
+# num = 0
+# t_num = len(df.values)
+# for rule in arules:
+#     num1=0
+#     num2=0
+#     num3=0
+
+#     for t in df.values:
+#         # print(set(rule[0]), set(rule[1]), set(t))
+#         t1=False
+#         t2=False
+#         if len(set(rule[0]).intersection(set(t))) == len(rule[0]):
+#             t1 = True
+#             num1+=1
+#         if len(set(rule[1]).intersection(set(t))) == len(rule[1]):
+#             num2+=1
+#             t2 = True
+#         if t1 and t2:
+#             num3+=1
+#     # print(num1, num2, num3)
+#     sup1 = num1/t_num
+#     sup2 = num2/t_num
+#     # print(num1,num2)
+#     sup = num3/t_num
+#     conf = num3/num1
+#     lift = conf*t_num/num2
+#     if sup >= MIN_SUPPORT and conf >= MIN_CONFIDENCE:
+#         num+=1
+#     print(lift, rule[2])
+    
+# print(num)
